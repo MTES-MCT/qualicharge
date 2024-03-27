5e62436d5406403a2e20c5abfc2dc09e30d7867e
Julien Maupetit <julien@maupetit.net>
2024-03-27 18:49:08 +0100

    🔧(api) update start up script
    We needed more flexibility to handle development vs production run.

---
+++ src/api/start.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+# We may expect that the following environment variables are set:
+#
+#   - PORT: the server port
+#   - QUALICHARGE_DEBUG: activate the debug mode (for development) [*]
+#
+# [*] optional
+
+set -eo pipefail
+
+declare -i debug=${QUALICHARGE_DEBUG:-0}
+declare -a extra_opts
+
+if [ ${debug} == 1 ]; then
+  extra_opts=(
+    "--reload" \
+    "--log-config logging-config.dev.yaml"
+  )
+  echo "⚗️ DEBUG mode activated. We hope your are not running in production. 🤞"
+else
+  extra_opts=(
+    "--log-config logging-config.prod.yaml"
+  )
+fi
+
+# shellcheck disable=SC2068
+uvicorn \
+  qualicharge.api:app \
+  --proxy-headers \
+  --host 0.0.0.0 \
+  --port "${PORT}" \
+  ${extra_opts[@]}
