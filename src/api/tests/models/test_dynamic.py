af8fc69289e847a606a929dd796717a8f924079d
Julien Maupetit <julien@maupetit.net>
2024-04-11 11:50:29 +0200

    ✨(api) add sessions dynamic router and model
    For now we only design the API.

--- src/api/tests/models/test_dynamic.py
+++ src/api/tests/models/test_dynamic.py
@@ -2,7 +2,15 @@
 
 from datetime import datetime
 
-from qualicharge.factories.dynamic import StatusFactory
+from qualicharge.factories.dynamic import SessionFactory, StatusFactory
+
+
+def test_session_model():
+    """Test the dynamic Session model."""
+    session = SessionFactory.build()
+
+    assert session.start < datetime.now()
+    assert session.end < datetime.now()
 
 
 def test_status_model():
