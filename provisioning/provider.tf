b0f52991c441c79ddf96746ce3bf1dd4255f2f25
Julien Maupetit <julien@maupetit.net>
2024-06-17 17:45:34 +0200

    🔨(project) provision metabase instance using terraform
    Using `make bootstrap` now automates Metabase instance provisioning:
    
    - metabase admin user is created
    - QualiCharge API database is set as a datasource

---
+++ provisioning/provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    metabase = {
+      source = "bnjns/metabase"
+      version = "0.9.0"
+    }
+  }
+}
+
+provider "metabase" {
+  # Configured using environment variables
+  # See env.d/terraform
+}
