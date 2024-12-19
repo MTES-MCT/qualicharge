9e52c912abb86a8d29cd4dfa70fd3e2710375c12
ssorin <ssorin@dwdlc.com>
2024-12-19 15:10:36 +0100

    🚨(dashboard) improve djlint execution speed
    Embed svgs as images (with <img>) in templates rather than directly in code (with <svg>).
    Removed unused consent form JavaScript for toggling checkboxes, as it is no longer required.

--- src/dashboard/apps/consent/static/consent/js/app.js
+++
@@ -1,8 +0,0 @@
-/**
- * check/uncheck all checkbox in consent form
- */
-document.getElementById("toggle-all")
-  .addEventListener("change", function() {
-    const checkboxes = document.getElementsByName("status");
-    checkboxes.forEach(checkbox => checkbox.checked = this.checked);
-});
