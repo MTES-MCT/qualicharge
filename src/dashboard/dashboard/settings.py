f0136b7f9b89865ca9e995eabc8e8054048d375f
ssorin <ssorin@dwdlc.com>
2025-01-06 17:31:01 +0100

    ✨(dashboard) add Sentry sampling rate configuration for traces and profiles
    Introduce environment variables `DASHBOARD_SENTRY_TRACES_SAMPLE_RATE` and `DASHBOARD_SENTRY_PROFILES_SAMPLE_RATE` to manage sampling rates for Sentry traces and profiles.

--- src/dashboard/dashboard/settings.py
+++ src/dashboard/dashboard/settings.py
@@ -28,6 +28,8 @@ env = environ.Env(
     DASHBOARD_DEBUG=(bool, False),
     DASHBOARD_ALLOWED_HOSTS=(list, ["localhost"]),
     DASHBOARD_SENTRY_DSN=(str, ""),
+    DASHBOARD_SENTRY_TRACES_SAMPLE_RATE=(float, 1.0),
+    DASHBOARD_SENTRY_PROFILES_SAMPLE_RATE=(float, 1.0),
 )
 env.prefix = "DASHBOARD_"
 
@@ -168,7 +170,12 @@ sentry_sdk.init(
     # Set traces_sample_rate to 1.0 to capture 100%
     # of transactions for performance monitoring.
     # We recommend adjusting this value in production.
-    traces_sample_rate=1.0,
+    traces_sample_rate=env.float("SENTRY_TRACES_SAMPLE_RATE"),
+    # To set a uniform sample rate
+    # Set profiles_sample_rate to 1.0 to profile 100%
+    # of sampled transactions.
+    # We recommend adjusting this value in production
+    profiles_sample_rate=env.float("SENTRY_PROFILES_SAMPLE_RATE"),
     # If you wish to associate users to errors (assuming you are using
     # django.contrib.auth) you may enable sending PII data.
     send_default_pii=True,
