e6d90a3a1c8437a7fac736f8bae8cab5d57974e0
ssorin <ssorin@dwdlc.com>
2025-01-27 17:38:46 +0100

    🚸(dashboard) refactor and modularize consent management templates
    Split and reorganized the manage consent template into reusable modular components.
    Introduced new partial templates for representative and company info, authorizations, and consent date/place.
    Retrieves and displays data of entity, user, control_authority and date and place of signature.
    Added contractual texts and translates into French.

--- src/dashboard/apps/consent/forms.py
+++ src/dashboard/apps/consent/forms.py
@@ -12,15 +12,102 @@ class ConsentCheckboxInput(CheckboxInput):
 
 
 class ConsentForm(forms.Form):
-    """Save user consent through a checkbox field."""
+    """Save user consent through a checkbox field.
 
+    Note: all texts of this form (attributes: `label`, `description`, `help_text`)
+    are intended to appear in a contract and must therefore be in French and
+    non-translatable.
+    """
+
+    # Specific authorisation checkbox
+    is_authorized_signatory = forms.BooleanField(
+        required=True,
+        initial=False,
+        widget=ConsentCheckboxInput(
+            attrs={
+                "label": "Le signataire du présent formulaire déclare être dûment "
+                "habilité par le client pour la signature du présent "
+                "document.",
+            },
+        ),
+    )
+
+    allows_measurements = forms.BooleanField(
+        required=True,
+        initial=False,
+        widget=ConsentCheckboxInput(
+            attrs={
+                "label": "L'historique des mesures, en kWh,",
+                "description": "du site (et puissances atteintes et dépassements "
+                "de puissance) : sur la période souhaitée, de 36 mois "
+                "maximum à compter de la date de la demande "
+                "(période limitée à la date de début du contrat)",
+            },
+        ),
+    )
+    allows_daily_index_readings = forms.BooleanField(
+        required=True,
+        initial=False,
+        widget=ConsentCheckboxInput(
+            attrs={
+                "label": "L'historique des relevés d'index quotidiens, en kWh, ",
+                "description": "du site; sur la période souhaitée, de 36 mois maximum "
+                "à compter de la date de la demande (période limitée "
+                "à la date de début du contrat)",
+            },
+        ),
+    )
+    allows_max_daily_power = forms.BooleanField(
+        required=True,
+        initial=False,
+        widget=ConsentCheckboxInput(
+            attrs={
+                "label": "L'historique de la puissance maximale quotidienne, en kVA "
+                "ou kWh,",
+                "description": "du site ; sur la période souhaitée, de 36 mois maximum "
+                "à compter de la date de la demande (période limitée à "
+                "la date de début du contrat)",
+            },
+        ),
+    )
+    allows_load_curve = forms.BooleanField(
+        required=True,
+        initial=False,
+        widget=ConsentCheckboxInput(
+            attrs={
+                "label": "L'historique de courbe de charge, aux pas restitués "
+                "par Enedis,",
+                "description": "du site<sup>1</sup>; sur la période souhaitée, "
+                "de 24 mois maximum "
+                "à compter de la date de la demande (période limitée à "
+                "la date de début du contrat)",
+                "help_text": "<sup>1</sup> Ensembles de valeurs moyennes horodatées "
+                "de la puissance active ou réactive soutirée, sur des "
+                "périodes d'intégrations consécutives et de même durée.",
+            },
+        ),
+    )
+    allows_technical_contractual_data = forms.BooleanField(
+        required=True,
+        initial=False,
+        widget=ConsentCheckboxInput(
+            attrs={
+                "label": "Les données techniques et contractuelles disponibles ",
+                "description": "du site ; Caractéristiques du raccordement, du "
+                "dispositif de comptage et des informations "
+                "contractuelles (option tarifaire, puissance "
+                "souscrite...)",
+            },
+        ),
+    )
+
+    # Global authorisation checkbox - this field must be in last position.
     consent_agreed = forms.BooleanField(
         required=True,
         initial=False,
         widget=ConsentCheckboxInput(
             attrs={
-                "label": _("I agree to give my consent"),
-                "help_text": _("Please confirm your consent by checking this box."),
+                "label": _("J'accepte de donner mon consentement"),
             },
         ),
     )
