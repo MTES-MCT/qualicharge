28fa70f41117201d401cd68e3fcc0562ff922ca3
ssorin <ssorin@dwdlc.com>
2025-01-27 16:33:50 +0100

    fixup! 🚧(dashboard) add consent validation handling and extend bulk update logic

--- src/dashboard/apps/consent/forms.py
+++ src/dashboard/apps/consent/forms.py
@@ -14,6 +14,64 @@ class ConsentCheckboxInput(CheckboxInput):
 class ConsentForm(forms.Form):
     """Save user consent through a checkbox field."""
 
+    # Specific authorisation checkbox
+    is_authorized_signatory = forms.BooleanField(
+        required=True,
+        initial=False,
+        widget=ConsentCheckboxInput(
+            attrs={
+                "label": _("the signatory is authorized"),
+            },
+        ),
+    )
+
+    allows_measurements = forms.BooleanField(
+        required=True,
+        initial=False,
+        widget=ConsentCheckboxInput(
+            attrs={
+                "label": _("allows historical measurements in kWh"),
+            },
+        ),
+    )
+    allows_daily_index_readings = forms.BooleanField(
+        required=True,
+        initial=False,
+        widget=ConsentCheckboxInput(
+            attrs={
+                "label": _("allow history of daily index readings in kWh")
+            },
+        ),
+    )
+    allows_max_daily_power = forms.BooleanField(
+        required=True,
+        initial=False,
+        widget=ConsentCheckboxInput(
+            attrs={
+                "label": _("allows historical maximum daily power in kVa or kWh "),
+            },
+        ),
+    )
+    allows_load_curve = forms.BooleanField(
+        required=True,
+        initial=False,
+        widget=ConsentCheckboxInput(
+            attrs={
+                "label": _("allows history of load curve, at steps returned by Enedis"),
+            },
+        ),
+    )
+    allows_technical_contractual_data = forms.BooleanField(
+        required=True,
+        initial=False,
+        widget=ConsentCheckboxInput(
+            attrs={
+                "label": _("allows the technical and contractual data available"),
+            },
+        ),
+    )
+
+    # Global authorisation checkbox
     consent_agreed = forms.BooleanField(
         required=True,
         initial=False,
