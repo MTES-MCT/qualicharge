2bd0113ec91f228c489e49ee65dc912dfc0d895c
philippe thomy <philippe.thomy@beta.gouv.fr>
2025-02-18 00:59:36 +0100

    new version

--- src/prefect/tests/infrastructure/test_t1.py
+++ src/prefect/tests/infrastructure/test_t1.py
@@ -3,66 +3,31 @@
 T1: the number of publicly open points of charge by power level.
 """
 
-import pandas as pd  # type: ignore
+from datetime import datetime
+
 import pytest  # type: ignore
 from sqlalchemy import text
 
 from indicators.infrastructure import t1  # type: ignore
-from indicators.models import IndicatorPeriod, Level  # type: ignore
+from indicators.models import IndicatorPeriod, IndicatorTimeSpan, Level  # type: ignore
 
-PARAMETERS_FLOW = [
-    (
-        Level.CITY,
-        "SELECT COUNT(*) FROM City",
-        ["75056", "13055", "69123"],
-        212,
-    ),
-    (
-        Level.EPCI,
-        "SELECT COUNT(*) FROM EPCI",
-        ["200054781", "200054807", "200046977"],
-        2250,
-    ),
-    (
-        Level.DEPARTMENT,
-        "SELECT COUNT(*) FROM Department",
-        ["59", "75", "13"],
-        1489,
-    ),
-    (
-        Level.REGION,
-        "SELECT COUNT(*) FROM Region",
-        ["11", "84", "75"],
-        8724,
-    ),
-]
-PARAMETERS_GET_VALUES = [
-    (
-        Level.CITY,
-        "SELECT id FROM City WHERE name IN ('Paris', 'Marseille', 'Lyon')",
-        212,
-    ),
-    (
-        Level.EPCI,
-        "SELECT id FROM EPCI WHERE code IN ('200054781', '200054807', '200046977')",
-        2250,
-    ),
-    (
-        Level.DEPARTMENT,
-        "SELECT id FROM Department WHERE code IN ('59', '75', '13')",
-        1489,
-    ),
-    (
-        Level.REGION,
-        "SELECT id FROM Region WHERE code IN ('11', '84', '75')",
-        8724,
-    ),
-]
-PARAMETERS_CHUNK = [10, 50, 100, 500]
-PERIOD = IndicatorPeriod.DAY
+from ..param_tests import (
+    PARAM_FLOW,
+    PARAM_VALUE,
+    PARAMETERS_CHUNK,
+)
+
+# expected result
+N_LEVEL = [212, 2250, 1489, 8724]
+N_DPTS = 109
+N_NAT_REG_DPT_EPCI_CITY = 36465
+
+TIMESPAN = IndicatorTimeSpan(start=datetime.now(), period=IndicatorPeriod.DAY)
+PARAMETERS_FLOW = [prm + (lvl,) for prm, lvl in zip(PARAM_FLOW, N_LEVEL, strict=True)]
+PARAMETERS_VALUE = [prm + (lvl,) for prm, lvl in zip(PARAM_VALUE, N_LEVEL, strict=True)]
 
 
-@pytest.mark.parametrize("level,query,expected", PARAMETERS_GET_VALUES)
+@pytest.mark.parametrize("level,query,expected", PARAMETERS_VALUE)
 def test_task_get_values_for_target(db_connection, level, query, expected):
     """Test the `get_values_for_target` task."""
     result = db_connection.execute(text(query))
@@ -81,8 +46,7 @@ def test_task_get_values_for_target_unexpected_level(db_connection):
 @pytest.mark.parametrize("level,query,targets,expected", PARAMETERS_FLOW)
 def test_flow_t1_for_level(db_connection, level, query, targets, expected):
     """Test the `t1_for_level` flow."""
-    now = pd.Timestamp.now()
-    indicators = t1.t1_for_level(level, PERIOD, now, chunk_size=1000)
+    indicators = t1.t1_for_level(level, TIMESPAN, chunk_size=1000)
     # assert len(indicators) == db_connection.execute(text(query)).scalars().one()
     assert indicators.loc[indicators["target"].isin(targets), "value"].sum() == expected
 
@@ -91,8 +55,7 @@ def test_flow_t1_for_level(db_connection, level, query, targets, expected):
 def test_flow_t1_for_level_with_various_chunk_sizes(chunk_size):
     """Test the `t1_for_level` flow with various chunk sizes."""
     level, query, targets, expected = PARAMETERS_FLOW[2]
-    now = pd.Timestamp.now()
-    indicators = t1.t1_for_level(level, PERIOD, now, chunk_size=chunk_size)
+    indicators = t1.t1_for_level(level, TIMESPAN, chunk_size=chunk_size)
     assert indicators.loc[indicators["target"].isin(targets), "value"].sum() == expected
 
 
@@ -100,24 +63,30 @@ def test_flow_t1_national(db_connection):
     """Test the `t1_national` flow."""
     query = "SELECT COUNT(*) FROM PointDeCharge WHERE puissance_nominale::numeric >= 0"
     expected = db_connection.execute(text(query)).scalars().one()
-    indicators = t1.t1_national(PERIOD, pd.Timestamp.now())
+    indicators = t1.t1_national(TIMESPAN)
     assert indicators["value"].sum() == expected
 
 
-def test_flow_calculate(db_connection):
+def test_flow_t1_calculate(db_connection):
     """Test the `calculate` flow."""
-    now = pd.Timestamp.now()
-    expected = sum(
+    """expected = sum(
         [
-            t1.t1_for_level(Level.CITY, PERIOD, now, chunk_size=1000)["value"].sum(),
-            t1.t1_for_level(Level.EPCI, PERIOD, now, chunk_size=1000)["value"].sum(),
-            t1.t1_for_level(Level.DEPARTMENT, PERIOD, now, chunk_size=1000)[
-                "value"
-            ].sum(),
-            t1.t1_for_level(Level.REGION, PERIOD, now, chunk_size=1000)["value"].sum(),
-            t1.t1_national(PERIOD, now)["value"].sum(),
+            t1.t1_for_level(Level.CITY, TIMESPAN, chunk_size=1000)["value"].sum(),
+            t1.t1_for_level(Level.EPCI, TIMESPAN, chunk_size=1000)["value"].sum(),
+            t1.t1_for_level(Level.DEPARTMENT, TIMESPAN, chunk_size=1000)["value"].sum(),
+            t1.t1_for_level(Level.REGION, TIMESPAN, chunk_size=1000)["value"].sum(),
+            t1.t1_national(TIMESPAN)["value"].sum(),
         ]
+    )"""
+    all_levels = [
+        Level.NATIONAL,
+        Level.REGION,
+        Level.DEPARTMENT,
+        Level.CITY,
+        Level.EPCI,
+    ]
+    indicators = t1.calculate(
+        TIMESPAN, all_levels, create_artifact=True, format_pd=True
     )
-    indicators = t1.calculate(PERIOD, create_artifact=True)
-    pd_indics = pd.DataFrame.from_records([indic.model_dump() for indic in indicators])
-    assert pd_indics["value"].sum() == expected
+    # assert indicators["value"].sum() == expected
+    assert list(indicators["level"].unique()) == all_levels
