e1988383c45838384a85f82dc20c3b773190d6f8
Julien Maupetit <julien@maupetit.net>
2025-02-17 17:29:25 +0100

    Add base indicator schema

---
+++ src/prefect/indicators/schemas.py
@@ -0,0 +1,33 @@
+"""QualiCharge prefect indicators: schemas."""
+
+from datetime import datetime
+from typing import Optional
+from uuid import uuid4
+
+from sqlalchemy.dialects.postgresql import JSONB, UUID
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.types import DateTime, Float, SmallInteger, String
+
+
+class BaseIndicator(DeclarativeBase):
+    """Base indicator model."""
+
+    id: Mapped[UUID] = mapped_column(
+        UUID(as_uuid=True), primary_key=True, default=uuid4
+    )
+    category: Mapped[str] = mapped_column(String(100))
+    code: Mapped[str] = mapped_column(String(5))
+    level: Mapped[int] = mapped_column(SmallInteger)
+    target: Mapped[Optional[str]] = mapped_column(String(100))
+    period: Mapped[str] = mapped_column(String(1))
+    value: Mapped[float] = mapped_column(Float)
+    extras: Mapped[Optional[dict]] = mapped_column(JSONB)
+    timestamp: Mapped[datetime] = mapped_column(DateTime(timezone=True))
+
+
+class Staging(BaseIndicator):
+    """Indicators running on the Staging API database."""
+
+
+class Production(BaseIndicator):
+    """Indicators running on the Staging API database."""
