aa8a4e7a254cdfcdd4d46feb442cb5ce1e8d23bf
Julien Maupetit <julien@maupetit.net>
2025-02-27 19:06:43 +0100

    Improve semantic

--- src/prefect/quality/flows.py
+++ src/prefect/quality/flows.py
@@ -1,21 +1,33 @@
 """Prefect flow: run checkpoints."""
 
 import great_expectations as gx
-from checkpoints.full import get_full_checkpoint
+from checkpoints.static import get_static_checkpoint
+
+api_data_source_name = "api-{environment}"
 
 
-def add_data_source(context, environment: str):
+def set_data_source(context, environment: str):
     """Add API data instance as a data source."""
     context.data_sources.add_postgres(
-        name=environment,
+        name=api_data_source_name.format(environment),
         connection_string=f"${{QUALICHARGE_API_DATABASE_URLS__{environment}}}",
     )
 
 
-def add_data_asset(context, environment: str, table: str, name: str):
+def get_data_asset(
+    context,
+    environment: str,
+    name: str,
+    table: str | None = None,
+    query: str | None = None,
+):
     """Get data asset for an environment."""
-    data_source = context.data_sources.get(environment)
-    return data_source.add_table_asset(table_name=table, name=name)
+    data_source = context.data_sources.get(api_data_source_name.format(environment))
+    if table:
+        return data_source.add_table_asset(name=name, table_name=table)
+    elif query:
+        return data_source.add_query_asset(name=name, query=query)
+    raise Exception
 
 
 def check_api_db_instance_quality():
@@ -23,13 +35,12 @@ def check_api_db_instance_quality():
     context = gx.get_context(mode="ephemeral")
 
     environment = "development"
-    add_data_source(context, environment)
-
-    table = "PointDeCharge"
-    asset = "pdc"
-    pdc = add_data_asset(context, environment, table, asset)
-    batch = pdc.add_batch_definition_whole_table(name="FULL_TABLE")
-    checkpoint = get_full_checkpoint(context, batch)
+    table = "statique"
+    asset = "static"
+    set_data_source(context, environment)
+    static = get_data_asset(context, environment, asset, table=table)
+    batch = static.add_batch_definition_whole_table(name="FULL_TABLE")
+    checkpoint = get_static_checkpoint(context, batch)
     checkpoint.run()
 
 
