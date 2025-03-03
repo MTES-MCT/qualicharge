c583c0cb8ec573445f9ac3180a841c7167508880
Julien Maupetit <julien@maupetit.net>
2025-03-03 18:00:30 +0100

    Add more expectations and by amenageur flow

--- src/prefect/quality/flows.py
+++ src/prefect/quality/flows.py
@@ -1,7 +1,16 @@
 """Prefect flow: run checkpoints."""
 
+import os
+
 import great_expectations as gx
+from prefect import flow, task
+from prefect.artifacts import create_markdown_artifact
+from prefect.cache_policies import NONE
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import Session
+
 from checkpoints.static import get_static_checkpoint
+from expectations.static import set_expectation_suite as set_static_expectation_suite
 
 api_data_source_name = "api-{environment}"
 
@@ -9,7 +18,7 @@ api_data_source_name = "api-{environment}"
 def set_data_source(context, environment: str):
     """Add API data instance as a data source."""
     context.data_sources.add_postgres(
-        name=api_data_source_name.format(environment),
+        name=api_data_source_name.format(environment=environment),
         connection_string=f"${{QUALICHARGE_API_DATABASE_URLS__{environment}}}",
     )
 
@@ -22,7 +31,9 @@ def get_data_asset(
     query: str | None = None,
 ):
     """Get data asset for an environment."""
-    data_source = context.data_sources.get(api_data_source_name.format(environment))
+    data_source = context.data_sources.get(
+        api_data_source_name.format(environment=environment)
+    )
     if table:
         return data_source.add_table_asset(name=name, table_name=table)
     elif query:
@@ -30,19 +41,104 @@ def get_data_asset(
     raise Exception
 
 
-def check_api_db_instance_quality():
+def get_api_db_checkpoint(environment="development", report_by_email: bool = False):
     """Check API database instance quality."""
     context = gx.get_context(mode="ephemeral")
 
-    environment = "development"
     table = "statique"
-    asset = "static"
+    asset = f"static-{environment}"
+    name = f"static-{environment}"
     set_data_source(context, environment)
+    set_static_expectation_suite(context)
     static = get_data_asset(context, environment, asset, table=table)
     batch = static.add_batch_definition_whole_table(name="FULL_TABLE")
-    checkpoint = get_static_checkpoint(context, batch)
-    checkpoint.run()
+    checkpoint = get_static_checkpoint(
+        context, batch, name=name, report_by_email=report_by_email
+    )
+    return checkpoint
 
 
+@task(cache_policy=NONE)
+def run_api_db_checkpoint_for_amenageur(
+    context,
+    amenageur: str,
+    environment: str = "development",
+    report_by_email: bool = False,
+):
+    """Get API database instance checkpoint for an amenageur."""
+    asset = f"static-{environment}-{amenageur}"
+    name = f"static-{environment}-{amenageur}"
+
+    query = f"SELECT * FROM STATIQUE where nom_amenageur = '{amenageur}'"
+    static = get_data_asset(context, environment, asset, query=query)
+    batch = static.add_batch_definition_whole_table(name="FULL_TABLE")
+    checkpoint = get_static_checkpoint(
+        context, batch, name=name, report_by_email=report_by_email
+    )
+    return checkpoint.run()
+
+
+@flow(log_prints=True)
+def run_api_db_validation(environment, report_by_email: bool = False):
+    """Run API DB checkpoint."""
+    checkpoint = get_api_db_checkpoint(environment, report_by_email=report_by_email)
+    result = checkpoint.run()
+    create_markdown_artifact(
+        "\n".join(
+            (
+                f"# Checkpoint validation: {checkpoint.name}",
+                "```json",
+                f"{result.describe()}",
+                "```",
+            )
+        ),
+        description=f"GX validation for API DB instance: {environment}",
+        key=f"api-db-static-{environment}",
+    )
+
+
+@flow(log_prints=True)
+def run_api_db_validation_by_amenageur(environment: str, report_by_email: bool = False):
+    """Run API DB checkpoint by amenageur."""
+    context = gx.get_context(mode="ephemeral")
+    set_data_source(context, environment)
+    set_static_expectation_suite(context)
+    md = [
+        "# Checkpoint validation by amenageur",
+        "| Amenageur | Statique |",
+        "| -- | -- |",
+    ]
+
+    db_connection_url = os.getenv(f"QUALICHARGE_API_DATABASE_URLS__{environment}")
+    db_engine = create_engine(db_connection_url)
+    with Session(db_engine) as session:
+        amenageurs = [
+            res[0]
+            for res in session.execute(
+                text(
+                    "SELECT nom_amenageur "
+                    "FROM Statique "
+                    "WHERE nom_amenageur <> ''"
+                    "GROUP BY nom_amenageur "
+                    "ORDER BY nom_amenageur"
+                )
+            ).all()
+        ]
+    for amenageur in amenageurs:
+        result = run_api_db_checkpoint_for_amenageur(
+            context, amenageur, environment, report_by_email=report_by_email
+        )
+        md.append(f"| {amenageur} | " + ("✅" if result.success else "😡") + " |")
+
+    create_markdown_artifact(
+        "\n".join(md),
+        description=f"GX validation by `Amenageur` for API DB instance: {environment}",
+        key=f"api-db-static-amenageurs-{environment}",
+    )
+
+
+# FIXME
+# Used to ease development, should be removed
 if __name__ == "__main__":
-    check_api_db_instance_quality()
+    # run_api_db_validation("development")
+    run_api_db_validation_by_amenageur("development")
