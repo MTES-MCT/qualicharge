172e334f91fab7bc82df69aff230083e4c32bee1
Julien Maupetit <julien@maupetit.net>
2025-03-03 18:00:30 +0100

    Add more expectations and by amenageur flow

--- src/prefect/quality/checkpoints/static.py
+++ src/prefect/quality/checkpoints/static.py
@@ -1,32 +1,31 @@
 """Checkpoints: full database expectations scan."""
 
 import great_expectations as gx
-import great_expectations.expectations as gxe
-
-from expectations.pdc import get_expectation_suite
+from expectations.static import NAME as STATIC_SUITE_NAME
 
 
-def get_static_checkpoint(context, batch, name="static"):
+def get_static_checkpoint(context, batch, name="static", report_by_email: bool = False):
     """Get the static checkpoint."""
-    definition_name = "static_validation_definition"
-    validation_definition = gx.ValidationDefinition(
-        data=batch, suite=get_expectation_suite(context), name=definition_name
-    )
+    # definition_name = "static_validation_definition"
+    suite = context.suites.get(STATIC_SUITE_NAME)
+    validation_definition = gx.ValidationDefinition(data=batch, suite=suite, name=name)
     validation_definition = context.validation_definitions.add(validation_definition)
 
-    action_list = [
-        gx.checkpoint.EmailAction(
-            notify_on="all",
-            name="Static expectations report",
-            receiver_emails="${GX_RECEIVER_EMAILS}",
-            smtp_address="${GX_BREVO_SMTP_ADDRESS}",
-            smtp_port="${GX_BREVO_SMTP_PORT}",
-            sender_login="${GX_BREVO_SENDER_LOGIN}",
-            sender_alias="Julien M.",
-            sender_password="${GX_BREVO_PASSWORD}",
-            use_tls=True,
+    action_list = []
+    if report_by_email:
+        action_list.append(
+            gx.checkpoint.EmailAction(
+                notify_on="all",
+                name="Static expectations report",
+                receiver_emails="${GX_RECEIVER_EMAILS}",
+                smtp_address="${GX_BREVO_SMTP_ADDRESS}",
+                smtp_port="${GX_BREVO_SMTP_PORT}",
+                sender_login="${GX_BREVO_SENDER_LOGIN}",
+                sender_alias="QualiCharge Bot <contact@qualicharge.beta.gouv.fr>",
+                sender_password="${GX_BREVO_PASSWORD}",
+                use_tls=True,
+            )
         )
-    ]
 
     # Create a check point
     checkpoint = gx.Checkpoint(
--- src/prefect/quality/expectations/static.py
+++ src/prefect/quality/expectations/static.py
@@ -3,6 +3,7 @@
 import great_expectations as gx
 import great_expectations.expectations as gxe
 
+NAME: str = "static"
 
 pdc_expectations = [
     gxe.ExpectColumnMinToBeBetween(
@@ -14,12 +15,24 @@ pdc_expectations = [
         max_value=4000,
     ),
 ]
+amenageur_expectations = [
+    gxe.ExpectColumnValuesToNotBeNull(column="nom_amenageur"),
+    gxe.ExpectColumnValuesToNotBeNull(column="siren_amenageur"),
+    gxe.ExpectColumnValuesToNotBeNull(column="contact_amenageur"),
+]
+operateur_expectations = [
+    gxe.ExpectColumnValuesToNotBeNull(column="nom_operateur"),
+    gxe.ExpectColumnValuesToNotBeNull(column="telephone_operateur"),
+]
 
 
-def get_expectation_suite(context, name="static"):
+def set_expectation_suite(context):
     """Get `static` expectations suite."""
-    expectation_suite = context.suites.add(gx.ExpectationSuite(name=name))
+    if NAME in context.suites.all():
+        return
+    expectation_suite = context.suites.add(gx.ExpectationSuite(name=NAME))
     expectations = pdc_expectations
+    expectations += amenageur_expectations
+    expectations += operateur_expectations
     for expectation in expectations:
         expectation_suite.add_expectation(expectation)
-    return expectation_suite
