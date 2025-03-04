79c4bbf3373e6174b539f846e6a2f181683908cd
Julien Maupetit <julien@maupetit.net>
2025-03-04 18:56:04 +0100

    add data docs

--- src/prefect/quality/checkpoints/static.py
+++ src/prefect/quality/checkpoints/static.py
@@ -4,7 +4,13 @@ import great_expectations as gx
 from expectations.static import NAME as STATIC_SUITE_NAME
 
 
-def get_static_checkpoint(context, batch, name="static", report_by_email: bool = False):
+def get_static_checkpoint(
+    context,
+    batch,
+    name="static",
+    report_by_email: bool = False,
+    update_data_docs: bool = False,
+):
     """Get the static checkpoint."""
     # definition_name = "static_validation_definition"
     suite = context.suites.get(STATIC_SUITE_NAME)
@@ -24,6 +30,27 @@ def get_static_checkpoint(context, batch, name="static", report_by_email: bool =
                 sender_alias="QualiCharge Bot <contact@qualicharge.beta.gouv.fr>",
                 sender_password="${GX_BREVO_PASSWORD}",
                 use_tls=True,
+            )
+        )
+
+    # This is the default path (relative to the root folder of the Data Context)
+    # but can be changed as required
+    base_directory = "/app/uncommitted/data_docs/local_site/"
+    site_config = {
+        "class_name": "SiteBuilder",
+        "site_index_builder": {"class_name": "DefaultSiteIndexBuilder"},
+        "store_backend": {
+            "class_name": "TupleFilesystemStoreBackend",
+            "base_directory": base_directory,
+        },
+    }
+    site_name = "my_data_docs_site"
+    if site_name not in context.list_data_docs_sites():
+        context.add_data_docs_site(site_name=site_name, site_config=site_config)
+    if update_data_docs:
+        action_list.append(
+            gx.checkpoint.actions.UpdateDataDocsAction(
+                name="update_my_site", site_names=[site_name]
             )
         )
 
--- src/prefect/quality/flows.py
+++ src/prefect/quality/flows.py
@@ -1,18 +1,45 @@
 """Prefect flow: run checkpoints."""
 
 import os
+import re
+import unicodedata
 
 import great_expectations as gx
+from indicators import db
 from prefect import flow, task
 from prefect.artifacts import create_markdown_artifact
 from prefect.cache_policies import NONE
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine, text, update
 from sqlalchemy.orm import Session
 
 from checkpoints.static import get_static_checkpoint
 from expectations.static import set_expectation_suite as set_static_expectation_suite
 
 api_data_source_name = "api-{environment}"
+
+
+def slugify(value, allow_unicode=False):
+    """Slugify a string.
+
+    Convert to ASCII if 'allow_unicode' is False. Convert spaces or repeated
+    dashes to single dashes. Remove characters that aren't alphanumerics,
+    underscores, or hyphens. Convert to lowercase. Also strip leading and
+    trailing whitespace, dashes, and underscores.
+
+    Taken from Django code base:
+    https://github.com/django/django/blob/main/django/utils/text.py
+    """
+    value = str(value)
+    if allow_unicode:
+        value = unicodedata.normalize("NFKC", value)
+    else:
+        value = (
+            unicodedata.normalize("NFKD", value)
+            .encode("ascii", "ignore")
+            .decode("ascii")
+        )
+    value = re.sub(r"[^\w\s-]", "", value.lower())
+    return re.sub(r"[-\s]+", "-", value).strip("-_")
 
 
 def set_data_source(context, environment: str):
@@ -34,14 +61,21 @@ def get_data_asset(
     data_source = context.data_sources.get(
         api_data_source_name.format(environment=environment)
     )
-    if table:
+    assets = [a.name for a in data_source.assets]
+    if name in assets:
+        return None
+    elif table:
         return data_source.add_table_asset(name=name, table_name=table)
     elif query:
         return data_source.add_query_asset(name=name, query=query)
     raise Exception
 
 
-def get_api_db_checkpoint(environment="development", report_by_email: bool = False):
+def get_api_db_checkpoint(
+    environment="development",
+    report_by_email: bool = False,
+    update_data_docs: bool = False,
+):
     """Check API database instance quality."""
     context = gx.get_context(mode="ephemeral")
 
@@ -53,7 +87,11 @@ def get_api_db_checkpoint(environment="development", report_by_email: bool = Fal
     static = get_data_asset(context, environment, asset, table=table)
     batch = static.add_batch_definition_whole_table(name="FULL_TABLE")
     checkpoint = get_static_checkpoint(
-        context, batch, name=name, report_by_email=report_by_email
+        context,
+        batch,
+        name=name,
+        report_by_email=report_by_email,
+        update_data_docs=update_data_docs,
     )
     return checkpoint
 
@@ -64,24 +102,36 @@ def run_api_db_checkpoint_for_amenageur(
     amenageur: str,
     environment: str = "development",
     report_by_email: bool = False,
+    update_data_docs: bool = False,
 ):
     """Get API database instance checkpoint for an amenageur."""
-    asset = f"static-{environment}-{amenageur}"
-    name = f"static-{environment}-{amenageur}"
+    asset = f"static-{environment}-{slugify(amenageur)}"
+    name = f"static-{environment}-{slugify(amenageur)}"
 
     query = f"SELECT * FROM STATIQUE where nom_amenageur = '{amenageur}'"
     static = get_data_asset(context, environment, asset, query=query)
+    # Duplicate
+    if not static:
+        return
     batch = static.add_batch_definition_whole_table(name="FULL_TABLE")
     checkpoint = get_static_checkpoint(
-        context, batch, name=name, report_by_email=report_by_email
+        context,
+        batch,
+        name=name,
+        report_by_email=report_by_email,
+        update_data_docs=update_data_docs,
     )
     return checkpoint.run()
 
 
 @flow(log_prints=True)
-def run_api_db_validation(environment, report_by_email: bool = False):
+def run_api_db_validation(
+    environment, report_by_email: bool = False, update_data_docs: bool = False
+):
     """Run API DB checkpoint."""
-    checkpoint = get_api_db_checkpoint(environment, report_by_email=report_by_email)
+    checkpoint = get_api_db_checkpoint(
+        environment, report_by_email=report_by_email, update_data_docs=update_data_docs
+    )
     result = checkpoint.run()
     create_markdown_artifact(
         "\n".join(
@@ -98,7 +148,9 @@ def run_api_db_validation(environment, report_by_email: bool = False):
 
 
 @flow(log_prints=True)
-def run_api_db_validation_by_amenageur(environment: str, report_by_email: bool = False):
+def run_api_db_validation_by_amenageur(
+    environment: str, report_by_email: bool = False, update_data_docs: bool = False
+):
     """Run API DB checkpoint by amenageur."""
     context = gx.get_context(mode="ephemeral")
     set_data_source(context, environment)
@@ -126,15 +178,19 @@ def run_api_db_validation_by_amenageur(environment: str, report_by_email: bool =
         ]
     for amenageur in amenageurs:
         result = run_api_db_checkpoint_for_amenageur(
-            context, amenageur, environment, report_by_email=report_by_email
+            context,
+            amenageur,
+            environment,
+            report_by_email=report_by_email,
+            update_data_docs=update_data_docs,
         )
-        md.append(f"| {amenageur} | " + ("✅" if result.success else "😡") + " |")
-
-    create_markdown_artifact(
-        "\n".join(md),
-        description=f"GX validation by `Amenageur` for API DB instance: {environment}",
-        key=f"api-db-static-amenageurs-{environment}",
-    )
+    #     md.append(f"| {amenageur} | " + ("✅" if result.success else "😡") + " |")
+    #
+    # create_markdown_artifact(
+    #     "\n".join(md),
+    #     description=f"GX validation by `Amenageur` for API DB instance: {environment}",
+    #     key=f"api-db-static-amenageurs-{environment}",
+    # )
 
 
 # FIXME
