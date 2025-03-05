4bd37e034d008fa0fe5fd7d58b53ddd5705f0e1d
ssorin <ssorin@dwdlc.com>
2025-03-05 14:34:24 +0100

    ✨(dashboard) add email notification for user validation
    Implemented functionality to send a validation email when a user's `is_validated` status changes to True.
    Added exception handling and Sentry logging for email sending failures.
    Updated settings.
    Added tests.

--- src/dashboard/apps/auth/models.py
+++ src/dashboard/apps/auth/models.py
@@ -1,5 +1,9 @@
 """Dashboard auth models."""
 
+import sentry_sdk
+from anymail.exceptions import AnymailRequestsAPIError
+from anymail.message import AnymailMessage
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.db.models import Q, QuerySet
@@ -25,6 +29,16 @@ class DashboardUser(AbstractUser):
         ),
     )
 
+    def save(self, *args, **kwargs):
+        """Override the save method to send a validation email."""
+        # Check if `is_validated` changes to True
+        if self.pk:
+            previous = DashboardUser.objects.filter(pk=self.pk).first()
+            if not previous.is_validated and self.is_validated:
+                self.send_validation_email()
+
+        super().save(*args, **kwargs)
+
     def get_entities(self) -> QuerySet[Entity]:
         """Get a list of entities, and their proxies associated."""
         return Entity.objects.filter(Q(users=self) | Q(proxies__users=self)).distinct()
@@ -32,3 +46,30 @@ class DashboardUser(AbstractUser):
     def can_validate_entity(self, entity: Entity) -> bool:
         """Determines if the provided entity can be validated."""
         return entity in self.get_entities()
+
+    def send_validation_email(self) -> None:
+        """Send a validation email to the user."""
+        email_to = self.email
+        email_config = settings.DASHBOARD_EMAIL_CONFIGS["validated_user"]
+        email_data = {
+            email_to: {
+                "last_name": self.last_name,  # type: ignore[union-attr]
+                "first_name": self.first_name,  # type: ignore[union-attr]
+                "link": email_config.get("link"),
+                "support_email": settings.CONTACT_EMAIL,
+            },
+        }
+
+        email = AnymailMessage(
+            to=[
+                email_to,
+            ],
+            template_id=email_config.get("template_id"),
+            merge_data=email_data,
+        )
+
+        try:
+            email.send()
+        except AnymailRequestsAPIError as e:
+            # fail silently and send a sentry log
+            sentry_sdk.capture_exception(e)
