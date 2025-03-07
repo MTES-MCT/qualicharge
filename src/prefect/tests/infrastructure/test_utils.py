afabdd9ee641a9ae93d4e6e6a980ee5256199710
philippe <philippe@loco-labs.io>
2025-03-07 19:05:45 +0100

    🔖(prefect) add e4 and up indicators
    calcul e4
    
    add e4
    
    tests e4
    
    add up function
    
    historicize_up
    
    tests up
    
    test up
    
    timespan t1
    
    timespan update
    
    update timespan
    
    update timespan
    
    validation up
    
    test up
    
    tests up
    
    tests up
    
    test up
    
    test up
    
    test up
    
    add i1
    
    Ruff, environment
    
    mypy
    
    add set_start function
    
    update
    
    ruff

--- src/prefect/tests/infrastructure/test_utils.py
+++ src/prefect/tests/infrastructure/test_utils.py
@@ -3,11 +3,14 @@
 common tests for all indicators.
 """
 
+from datetime import datetime, timedelta
+
 import pytest  # type: ignore
 
-from indicators.infrastructure import i1  # type: ignore
-from indicators.models import Level
-from indicators.types import Environment  # type: ignore
+from indicators.infrastructure import i1
+from indicators.models import IndicatorPeriod, Level
+from indicators.types import Environment
+from indicators.utils import set_start
 
 PARAMETERS_GET_TARGETS = [
     (Level.CITY, 35074),
@@ -27,3 +30,19 @@ def test_task_get_targets_for_level_unexpected_level():
     """Test the `get_targets_for_level` task (unexpected level)."""
     with pytest.raises(NotImplementedError, match="Unsupported level"):
         i1.get_targets_for_level.fn(Level.NATIONAL, Environment.TEST)
+
+
+def test_set_start():
+    """Test the 'set_start' function."""
+    tst = datetime(2024, 1, 10)
+    assert set_start(tst, -1, IndicatorPeriod.MONTH) == datetime(2023, 12, 1)
+    assert set_start(tst, -1, IndicatorPeriod.QUARTER) == datetime(2023, 10, 1)
+    assert set_start(tst, -1, IndicatorPeriod.WEEK) == datetime(2024, 1, 1)
+    assert set_start(tst, -2, IndicatorPeriod.WEEK) == datetime(2023, 12, 25)
+    assert set_start(tst, 0, IndicatorPeriod.DAY) == datetime(2024, 1, 10)
+    assert set_start(None, 0, IndicatorPeriod.DAY) - datetime.now() < timedelta(
+        minutes=5
+    )
+    assert set_start(tst, -1, IndicatorPeriod.DAY) == datetime(2024, 1, 9)
+    assert set_start(tst, -1, IndicatorPeriod.HOUR) == datetime(2024, 1, 9, 23)
+    assert set_start(tst, -1, IndicatorPeriod.YEAR) == datetime(2023, 1, 1)
