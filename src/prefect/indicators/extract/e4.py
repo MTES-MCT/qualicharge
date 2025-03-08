03505f9de5982e061f04724fc118e494eda7e850
philippe thomy <philippe.thomy@beta.gouv.fr>
2025-03-08 01:27:04 +0100

    calcul e4

--- src/prefect/indicators/extract/e4.py
+++ src/prefect/indicators/extract/e4.py
@@ -16,19 +16,20 @@ from prefect.task_runners import ThreadPoolTaskRunner
 from sqlalchemy import text
 from sqlalchemy.orm import Session
 
-from ..conf import settings
-from ..db import get_api_db_engine
-from ..models import Indicator, IndicatorTimeSpan, Level
-from ..types import Environment
-from ..utils import (
+from indicators.conf import settings
+from indicators.db import get_api_db_engine
+from indicators.models import Indicator, IndicatorTimeSpan, Level
+from indicators.types import Environment
+from indicators.utils import (
     export_indicators,
     get_num_for_level_query_params,
     get_targets_for_level,
+    get_timespan_filter_query_params,
 )
 
 LIST_POCS_FOR_LEVEL_QUERY_TEMPLATE = """
 SELECT
-    id_pdc_itinerance
+    id_pdc_itinerance,
     $level_id AS level_id
 FROM
     SESSION
@@ -39,8 +40,7 @@ FROM
     $join_extras
  WHERE
     $level_id IN ($indexes)
-    AND START >= $start - interval '30 days' 
-    AND START < $start
+    AND $timespan
 GROUP BY
     id_pdc_itinerance,
     $level_id
@@ -49,12 +49,16 @@ GROUP BY
 
 @task(task_run_name="values-for-target-{level:02d}", cache_policy=NONE)
 def get_values_for_targets(
-    level: Level, indexes: List[UUID], environment: Environment
+    level: Level,
+    timespan: IndicatorTimeSpan,
+    indexes: List[UUID],
+    environment: Environment,
 ) -> pd.DataFrame:
     """Fetch points of charge given input level and target index."""
-    query_template = Template(NUM_POCS_FOR_LEVEL_QUERY_TEMPLATE)
+    query_template = Template(LIST_POCS_FOR_LEVEL_QUERY_TEMPLATE)
     query_params: dict = {"indexes": ",".join(f"'{i}'" for i in map(str, indexes))}
     query_params |= get_num_for_level_query_params(level)
+    query_params |= get_timespan_filter_query_params(timespan, session=True)
     with Session(get_api_db_engine(environment)) as session:
         return pd.read_sql_query(
             query_template.substitute(query_params), con=session.connection()
@@ -72,6 +76,10 @@ def e4_for_level(
     chunk_size=settings.DEFAULT_CHUNK_SIZE,
 ) -> pd.DataFrame:
     """Calculate e4 for a level."""
+    timespan = IndicatorTimeSpan(
+        start=timespan.start - PeriodDuration[timespan.period.name].value,
+        period=timespan.period,
+    )
     if level == Level.NATIONAL:
         return e4_national(timespan, environment)
     targets = get_targets_for_level(level, environment)
@@ -82,14 +90,25 @@ def e4_for_level(
         else [ids.to_numpy()]
     )
     futures = [
-        get_values_for_targets.submit(level, chunk, environment)  # type: ignore[call-overload]
+        get_values_for_targets.submit(level, timespan, chunk, environment)  # type: ignore[call-overload]
         for chunk in chunks
     ]
     wait(futures)
 
     # Concatenate results and serialize indicators
     results = pd.concat([future.result() for future in futures], ignore_index=True)
-    merged = targets.merge(results, how="left", left_on="id", right_on="level_id")
+
+    grp = results.groupby("level_id")
+    results_list = pd.DataFrame()
+    results_list["level_id"] = [name for name, group in grp]
+    results_list["extras"] = [
+        {"id_pdc_itinerance": list(group["id_pdc_itinerance"])} for name, group in grp
+    ]
+    results_list["value"] = [
+        len(list(group["id_pdc_itinerance"])) for name, group in grp
+    ]
+
+    merged = targets.merge(results_list, how="left", left_on="id", right_on="level_id")
 
     # Build result DataFrame
     indicators = {
@@ -100,7 +119,9 @@ def e4_for_level(
         "period": timespan.period,
         "timestamp": timespan.start.isoformat(),
         "category": None,
-        "extras": None,
+        "extras": merged["extras"].fillna(
+            pd.Series([{"id_pdc_itinerance": []}] * len(merged))
+        ),
     }
     return pd.DataFrame(indicators)
 
@@ -152,3 +173,40 @@ def calculate(  # noqa: PLR0913
         indicators, environment, flow_name, description, create_artifact, persist
     )
     return indicators
+
+
+if __name__ == "__main__":
+    from datetime import datetime  # noqa: I001
+    from indicators.models import IndicatorPeriod, PeriodDuration
+
+    indexes = [1, 2, 3]
+    TIMESPAN = IndicatorTimeSpan(
+        start=datetime.now() - PeriodDuration.QUARTER.value,
+        period=IndicatorPeriod.QUARTER,
+    )
+    print(e4_for_level(Level.REGION, TIMESPAN, Environment.DEVELOPMENT))
+    """
+    query_template = Template(LIST_POCS_FOR_LEVEL_QUERY_TEMPLATE)
+    query_params: dict = {"indexes": ",".join(f"'{i}'" for i in map(str, indexes))}
+    query_params |= get_num_for_level_query_params(Level.DEPARTMENT)
+    query_params |= get_timespan_filter_query_params(TIMESPAN, session=True)
+    print(query_template.substitute(query_params))  # noqa: T201
+    """
+    """
+    all_levels = [
+        Level.NATIONAL,
+        Level.REGION,
+        # Level.DEPARTMENT,
+        # Level.CITY,
+        # Level.EPCI,
+    ]
+    calculate(
+        # IndicatorTimeSpan(start=datetime.now(), period=IndicatorPeriod.DAY),
+        IndicatorTimeSpan(
+            start=datetime.fromisoformat("2025-02-21"), period=IndicatorPeriod.DAY
+        ),
+        Environment.DEVELOPMENT,
+        all_levels,
+        persist=True,
+    )
+    """
