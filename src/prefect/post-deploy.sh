bc1eafa499ddc19f40659ae9cd7a9990ff274383
Julien Maupetit <julien@maupetit.net>
2025-03-17 12:07:21 +0100

    🚀(prefect) push deployment every hour
    We cannot submit deployments in a post-deployment script as there is a
    discrepency between the running server and the post-deployment script.
    To mitigate this we will create or update deployments in a cronjob
    running every hour.

--- src/prefect/post-deploy.sh
+++ src/prefect/post-deploy.sh
@@ -13,15 +13,3 @@ prefect server database upgrade -y
 # Create worker pool
 echo "👷 Will create 'indicators' worker pool…"
 prefect work-pool create --type process --overwrite indicators
-
-# Download yq
-echo "🌍 Will download and install YQ…"
-curl -sL "https://github.com/mikefarah/yq/releases/download/${YQ_VERSION}/${YQ_BINARY}.tar.gz" | \
-  tar xz -C /tmp
-
-# Create or update deployments
-echo "⚙️ Will create or update deployments…"
-for deployment in $(${YQ} .deployments.[].name < prefect.yaml); do
-  echo "🚀 deployment ${deployment}"
-  prefect deploy -n "${deployment}"
-done
