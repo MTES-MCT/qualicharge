0a8b9ac9b555e16dee0bce951dbe25bf9f11c499
Julien Maupetit <julien@maupetit.net>
2025-03-27 12:34:06 +0100

    moving some async auth tests

--- src/api/tests/conftest.py
+++ src/api/tests/conftest.py
@@ -5,11 +5,15 @@
 from qualicharge.auth.factories import IDTokenFactory
 
 from .fixtures.app import clear_lru_cache, client, client_auth
+from .fixtures.asynchronous import anyio_backend
 from .fixtures.cli import runner
 from .fixtures.db import (
+    db_async_engine,
+    db_async_session,
     db_engine,
     db_session,
     load_operational_units,
+    override_db_async_test_session,
     override_db_test_session,
     versioning_manager,
 )
