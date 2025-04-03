28c5b390fe5dfc354fa2745e385f400e3d5628ea
ssorin <ssorin@dwdlc.com>
2025-04-03 12:12:25 +0200

    🩹(dashboard) set UTC timezone for consent_start_date function
    Previously, the start date was timezone-naive, which could cause inconsistencies in environments expecting timezone-aware datetimes.

--- src/dashboard/apps/consent/tests/test_utils.py
+++ src/dashboard/apps/consent/tests/test_utils.py
@@ -12,7 +12,9 @@ def test_consent_start_date_current_year(patch_datetime_now):
     """Test `consent_start_date` returns the start date of the current year."""
     start_date = consent_start_date()
     current_year = datetime.datetime.now().year
-    expected_date = datetime.datetime(year=current_year, month=1, day=1)
+    expected_date = datetime.datetime(
+        year=current_year, month=1, day=1, tzinfo=datetime.timezone.utc
+    )
     assert start_date == expected_date
 
 
