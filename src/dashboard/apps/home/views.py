2a96b09d4def1569b1a2f0584a0a81637634b7c7
ssorin <ssorin@dwdlc.com>
2025-04-17 14:12:13 +0200

    🚸️(dashboard) add renewable energy card to dashboard
    Introduce a card for managing renewable energy production data on the dashboard, along with supporting templates and logic for badges. Refactor existing consent card and context to accommodate the new feature, ensuring both cards display pending details dynamically.

--- src/dashboard/apps/home/views.py
+++ src/dashboard/apps/home/views.py
@@ -16,15 +16,25 @@ class IndexView(UserValidationMixin, TemplateView):
 
         entities = self.request.user.get_entities()
         has_awaiting_consent = any(entity.get_consents() for entity in entities)
+        has_pending_renewable = True  # todo: add the logic
 
         if has_awaiting_consent:
-            context["top_detail"] = {
-                "badges": [
-                    {
-                        "extra_classes": "fr-badge--new",
-                        "label": _("Pending consents"),
-                    }
-                ]
-            }
+            label = _("Pending consents")
+            context["consent_top_detail"] = self._top_detail_context(label)
+
+        if has_pending_renewable:
+            label = _("Pending meters reading")
+            context["renewable_top_detail"] = self._top_detail_context(label)
 
         return context
+
+    @staticmethod
+    def _top_detail_context(label):
+        return {
+            "badges": [
+                {
+                    "extra_classes": "fr-badge--new",
+                    "label": label,
+                }
+            ]
+        }
