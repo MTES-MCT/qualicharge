382c80b421423401e4e86242e4d78f7fc7b281f0
ssorin <ssorin@dwdlc.com>
2025-04-18 15:28:57 +0200

    🚸(dashboard) add renewable meter reading management page
    Introduce a form view and template for managing renewable meter readings. Updated the URLs to include a `manage` endpoint with a redirect for unauthorized direct access and enhanced template components to streamline user interaction.

--- src/dashboard/apps/renewable/urls.py
+++ src/dashboard/apps/renewable/urls.py
@@ -1,11 +1,19 @@
 """Dashboard renewable meter app urls."""
 
 from django.urls import path
+from django.views.generic.base import RedirectView
 
-from .views import IndexView
+from .views import IndexView, RenewableMetterReadingFormView
 
 app_name = "renewable"
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
+    path("manage/<slug:slug>", RenewableMetterReadingFormView.as_view(), name="manage"),
+    # direct access to `manage/` is not allowed
+    path(
+        "manage/",
+        RedirectView.as_view(pattern_name="renewable:index", permanent=False),
+        name="manage",
+    ),
 ]
