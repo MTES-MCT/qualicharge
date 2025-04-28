57e639b73b04c5ee3633310601564b85815d15dd
ssorin <ssorin@dwdlc.com>
2025-04-28 17:16:07 +0200

    ✨(renewable) refactor renewable views and templates to support EntityMixin
    Implement EntityMixin for managing renewable meters and ensure proper context handling.
    Enhance template logic to support renewable_delivery_points and add a reusable "no data" card.
    Include tests for EntityMixin and renewable views.

--- src/dashboard/apps/renewable/views.py
+++ src/dashboard/apps/renewable/views.py
@@ -1,9 +1,11 @@
 """Dashboard renewable meter app views."""
 
+from django.conf import settings
 from django.urls import reverse_lazy as reverse
 from django.utils.translation import gettext_lazy as _
 from django.views.generic import TemplateView
 
+from apps.core.mixins import EntityMixin
 from apps.core.views import BaseView
 
 BREADCRUMB_CURRENT_LABEL = _("Renewable meter")
@@ -27,21 +29,26 @@ class IndexView(BaseView, TemplateView):
         return context
 
 
-class RenewableMetterReadingFormView(BaseView, TemplateView):
+class RenewableMetterReadingFormView(EntityMixin, BaseView, TemplateView):
     """Manage renewable meters."""
 
     template_name = "renewable/manage.html"
     success_url = reverse("renewable:index")
 
+    breadcrumb_current = _("Manage renewable meter reading")
     breadcrumb_links = [
         {"url": reverse("renewable:index"), "title": BREADCRUMB_CURRENT_LABEL},
     ]
-    breadcrumb_current = _("Manage renewable meter reading")
 
     def get_context_data(self, **kwargs):
-        """Add user's entities to the context."""
-        # todo : add logic
+        """Add custom attributes to the context."""
+        entity = self.get_entity()
+
         context = super().get_context_data(**kwargs)
-        context["entity"] = True
+        context["entity"] = entity
+        context["renewable_delivery_points"] = (
+            entity.get_unsubmitted_quarterly_renewables()
+        )
+        context["signature_location"] = settings.CONSENT_SIGNATURE_LOCATION
 
         return context
