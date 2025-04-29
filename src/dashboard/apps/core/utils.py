073cfa15c131831bd9368d2f34110f79363ff126
ssorin <ssorin@dwdlc.com>
2025-04-29 11:11:49 +0200

    ✨(renewable) update logic to handle previous quarter data
    Updated functions, methods, and placeholders to retrieve and validate data from the previous quarter instead of the current quarter.
    Introduced `get_previous_quarter_date_range` utility and adjusted date calculations.

--- src/dashboard/apps/core/utils.py
+++ src/dashboard/apps/core/utils.py
@@ -1,11 +1,15 @@
 """Dashboard core utils."""
 
-import datetime
+from datetime import date
+from typing import Tuple
 
 from dateutil.relativedelta import relativedelta
 from django.utils import timezone
 
 from apps.core.validators import validate_siret
+
+DateRange = Tuple[date, date]
+MONTHS_PER_QUARTER = 3
 
 
 def siret2siren(siret: str) -> str:
@@ -14,12 +18,23 @@ def siret2siren(siret: str) -> str:
     return siret[:9]
 
 
-def get_current_quarter_date_range() -> tuple[datetime.date, datetime.date]:
-    """Get the start and end day of the current quarter."""
-    now: datetime.date = timezone.now()
-
-    current_quarter = (now.month - 1) // 3
-    first_day: datetime.date = datetime.datetime(now.year, current_quarter * 3 + 1, 1)
-    last_day: datetime.date = first_day + relativedelta(months=3, days=-1)
+def get_quarter_date_range(reference_date: date) -> DateRange:
+    """Get the start and end day of a quarter for a given date."""
+    current_quarter = (reference_date.month - 1) // 3
+    first_day: date = date(reference_date.year, current_quarter * 3 + 1, 1)
+    last_day: date = first_day + relativedelta(months=3, days=-1)
 
     return first_day, last_day
+
+
+def get_current_quarter_date_range() -> DateRange:
+    """Get the start and end day of the current quarter."""
+    return get_quarter_date_range(timezone.now())
+
+
+def get_previous_quarter_date_range(reference_date: date) -> DateRange:
+    """Get the start and end day of the previous quarter of a given date."""
+    first_day, _ = get_quarter_date_range(reference_date)
+    previous_day = first_day - relativedelta(days=1)
+
+    return get_quarter_date_range(previous_day)
