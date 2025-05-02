d3f727c0a17c52d479c7d29f4fc0a0acc2af9726
Julien Maupetit <julien@maupetit.net>
2025-05-02 16:53:05 +0200

    🐛(prefect) fix offline workers cleanup script
    Worker id and the UUID in its name are different.

--- src/prefect/clean-offline-workers.sh
+++ src/prefect/clean-offline-workers.sh
@@ -11,12 +11,13 @@ workers=$(curl -s \
   "${PREFECT_API_URL}work_pools/${work_pool}/workers/filter" \
   --header "Content-Type: application/json"  \
   --data '{"workers": {"status": {"any_": ["OFFLINE"]}}}' |\
-  jq -r .[].id)
+  jq -r .[].name |\
+  sed "s/ /%20/g")
 
 # Delete offline workers
 for worker in ${workers}; do
   echo "Deleting worker ${worker}…"
   curl -s \
     -X DELETE \
-    "${PREFECT_API_URL}work_pools/${work_pool}/workers/ProcessWorker%20${worker}"
+    "${PREFECT_API_URL}work_pools/${work_pool}/workers/${worker}"
 done
