99d6ad38da8582c4d5a509789a54ceecaf29c86b
ssorin <ssorin@dwdlc.com>
2025-05-13 15:49:43 +0200

    🚸(dashboard) add submitted renewables view with sorting and pagination
    Introduce a new view to display submitted renewable meter readings alongside template updates.
    Implement sorting by quarter and station name, add URL routing, and validate functionality with tests.
    Adjust textual labels for consistency across templates.

--- src/dashboard/apps/renewable/views.py
+++ src/dashboard/apps/renewable/views.py
@@ -1,5 +1,7 @@
 """Dashboard renewable meter app views."""
 
+from typing import Any
+
 from django.conf import settings
 from django.contrib import messages
 from django.core.exceptions import ValidationError
@@ -7,10 +9,11 @@ from django.db.models import QuerySet
 from django.forms import modelformset_factory
 from django.urls import reverse_lazy as reverse
 from django.utils.translation import gettext_lazy as _
-from django.views.generic import FormView, TemplateView
+from django.views.generic import FormView, ListView, TemplateView
 
 from apps.core.mixins import EntityViewMixin
 from apps.core.models import Entity
+from apps.core.utils import get_quarter_number
 from apps.core.views import BaseView
 from apps.renewable.forms import RenewableForm, RenewableFormSet
 from apps.renewable.models import Renewable
@@ -142,3 +145,56 @@ class RenewableMetterReadingFormView(EntityViewMixin, BaseView, FormView):
         """Handle invalid formset."""
         formset.non_form_errors().append(_("The form contains errors."))
         return self.render_to_response(self.get_context_data(formset=formset))
+
+
+class SubmittedRenewableView(EntityViewMixin, BaseView, ListView):
+    """Submitted renewable view."""
+
+    context_object_name = "renewables"
+    template_name = "renewable/submitted.html"
+
+    breadcrumb_current = _("Submitted renewable meter reading")
+    breadcrumb_links = [
+        {"url": reverse("renewable:index"), "title": BREADCRUMB_CURRENT_LABEL},
+    ]
+
+    def get_queryset(self):
+        """Filter queryset to only return submitted renewables for the current user."""
+        return self._order_by_quarter_stations(self.get_entity().get_renewables())
+
+    @staticmethod
+    def _order_by_quarter_stations(renewables: QuerySet) -> list[Any]:
+        """Sort and group stations alphabetically by name and by revert quarter.
+
+        Returns a sorted list of dictionaries with this structured
+        data, ordered by reversed quarter period date and by the first station name in a
+        case-insensitive manner.
+
+        Args:
+            renewables (QuerySet): A queryset of renewables objects.
+
+        Returns:
+            list[Any]: A sorted list of dictionaries, each containing structured data
+            about the renewable and its delivery point and stations.
+        """
+        structured_renewables = [
+            {
+                **renewable.__dict__,
+                "provider_assigned_id": renewable.delivery_point.provider_assigned_id,
+                "stations_grouped": renewable.delivery_point.get_linked_stations(),
+            }
+            for renewable in renewables
+        ]
+
+        return sorted(
+            structured_renewables,
+            key=lambda x: (
+                -x["collected_at"].year,  # reverse year
+                -get_quarter_number(x["collected_at"]),  # reverse quarter
+                (
+                    list(x["stations_grouped"].keys())[0].casefold()
+                    if x["stations_grouped"]
+                    else ""
+                ),
+            ),
+        )
