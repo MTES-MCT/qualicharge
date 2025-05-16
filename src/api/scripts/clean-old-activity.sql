8c2a4428745292f49112368912ecb51989397743
Julien Maupetit <julien@maupetit.net>
2025-05-16 15:50:16 +0200

    fixup! 🗃️(api) add activity data cleaning cronjob

--- src/api/scripts/clean-old-activity.sql
+++ src/api/scripts/clean-old-activity.sql
@@ -1,3 +1,4 @@
 DELETE
 FROM activity
-WHERE issued_at < CURRENT_DATE - interval :clean_before ; -- FIXME: should check variable is set
+WHERE issued_at < CURRENT_DATE - INTERVAL :clean_before ; -- FIXME: should check variable is set
+VACUUM (FULL, ANALYZE) activity; -- FIXME: VACUUM locks the table / db ?
