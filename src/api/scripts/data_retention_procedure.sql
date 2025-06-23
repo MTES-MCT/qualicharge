7f846e8789b28010c33a201a79e672278e1bcb20
Julien Maupetit <julien@maupetit.net>
2025-06-23 11:43:18 +0200

    🐛(api) fix timescaledb cleanup parameter
    Concatenating strings in a procedure call argument is not allowed. The
    easiest way to fix this is to set the `DYNAMIC_CLEAN_AFTER` environment
    variable to expected JSON argument.

--- src/api/scripts/data_retention_procedure.sql
+++ src/api/scripts/data_retention_procedure.sql
@@ -4,6 +4,6 @@
 -- REQUIREMENT:
 -- One should set the `drop_after` variable while calling this script
 --
--- psql --set=drop_after='2 months' "${DATABASE_URL}" -f scripts/data_retention_procedure.sql  
+-- psql --set=drop_after='{"drop_after":"20 days"}' "${DATABASE_URL}" -f scripts/data_retention_procedure.sql  
 --
-call generic_retention(config => '{"drop_after":' || :'drop_after' || '}');
+call generic_retention(config => :'drop_after');
