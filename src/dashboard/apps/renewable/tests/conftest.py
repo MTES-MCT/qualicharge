3fde79a45e2415b6fb5219ba82e59c4e2236de96
ssorin <ssorin@dwdlc.com>
2025-10-29 14:29:46 +0100

    ✨(dashboard) add session timeout configuration and browser close expiration
    Configured session timeout with `SESSION_COOKIE_AGE` and set sessions to expire on browser close.
    Updated related environment and workflow settings.

--- src/dashboard/apps/renewable/tests/conftest.py
+++ src/dashboard/apps/renewable/tests/conftest.py
@@ -7,3 +7,13 @@ import pytest
 def set_language_code(settings):
     """Fixture to set LANGUAGE_CODE to 'en-us' by default for tests."""
     settings.LANGUAGE_CODE = "en-us"
+
+
+@pytest.fixture()
+def disable_session_expiration(settings):
+    """Disable session lifetime during testing.
+
+    Since we often mock now() in tests, using it together with SESSION_COOKIE_AGE causes
+    the session to expire prematurely. Therefore, we assign it a long lifetime.
+    """
+    settings.SESSION_COOKIE_AGE = 999999999  # very very long lifespan
