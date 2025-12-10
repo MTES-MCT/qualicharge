ac26d9fee7fbe52ba18b924513b1cef08df26534
Julien Maupetit <julien@maupetit.net>
2025-12-10 17:17:41 +0100

    🐛(api) force Status and Session date time fields to be TZ-aware
    When checking that Status or Session respects configured maximal age
    restriction, if we compare naive and tz-aware datetime instances, the
    API returns an HTTP 500 error status.
    
    We now enforce submitted data to contain only TZ-aware datetimes.

--- src/api/qualicharge/models/dynamic.py
+++ src/api/qualicharge/models/dynamic.py
@@ -5,11 +5,13 @@ from enum import StrEnum
 from typing import Annotated, Optional
 
 from pydantic import AfterValidator, PositiveFloat, model_validator
-from pydantic.types import PastDatetime
+from pydantic.types import AwareDatetime, PastDatetime
 from sqlmodel import Field, SQLModel
 from typing_extensions import Self
 
 from ..conf import settings
+
+AwarePastDatetime = Annotated[AwareDatetime, PastDatetime]
 
 
 def not_older_than(value: datetime, max_age: timedelta):
@@ -72,7 +74,7 @@ class StatusCreate(StatusAPIBase):
     """Point of charge status create."""
 
     horodatage: Annotated[
-        PastDatetime,
+        AwarePastDatetime,
         AfterValidator(
             lambda v: not_older_than(v, timedelta(seconds=settings.API_MAX_STATUS_AGE))
         ),
@@ -108,8 +110,9 @@ class SessionCreate(SessionBase):
         },
     )
     start: Annotated[
-        PastDatetime,
+        AwarePastDatetime,
         AfterValidator(
             lambda v: not_older_than(v, timedelta(seconds=settings.API_MAX_SESSION_AGE))
         ),
     ]
+    end: AwarePastDatetime
